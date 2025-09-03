cf7a7fe74af9b913db5bb7f5c789e4f654303b25
AfifShaikh37 <afifshaikh.375@gmail.com>
2025-09-03 11:02:51 +0530

    Add files via upload

---
+++ Datatype.java
@@ -0,0 +1,30 @@
+public class Datatype {
+    public static void main(String[]args) {
+        String name = "Hey there !";
+        int myinteger = 505;
+        float myfloat = 3.147f;
+        byte mybyte = 100;
+        short myshort = 5000;
+        long mylong= 15000000000L;
+        double mydouble = 19.99d;
+        boolean mybool = true;
+        char mytext = 'h';
+        var auto = 007;
+
+        System.out.println(name);
+        System.out.println(myinteger);
+        System.out.println(myfloat);
+        System.out.println(mybyte);
+        System.out.println(myshort);
+        System.out.println(mylong);
+        System.out.println(mydouble);
+        System.out.println(mybool);
+        System.out.println(mytext);
+        System.out.println(auto);
+    }
+}
+        
+
+
+
+
